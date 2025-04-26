ef8a36e04624e031a3ab468dbca519330bca4855
nassipkaliv <nassipkaliv@gmail.com>
2025-04-26 19:42:53 +0500

    section done

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',   
+  spaceBetween: 30,        
+  speed: 100000,            
+  autoplay: {
+    delay: 0,              
+    disableOnInteraction: false, 
+  },
+  grabCursor: true,        
+  allowTouchMove: true,    
+  freeMode: false,         
+});
