e579334d3e44cbaa4de342658de87e5f7540c028
nassipkaliv <nassipkaliv@gmail.com>
2025-04-27 15:52:06 +0500

    update

--- js/app.js
+++ js/app.js
@@ -13,9 +13,12 @@ const swiper = new Swiper('.swiper', {
 
 window.addEventListener('scroll', function() {
   const header = document.getElementById('scrollHeaderMenu');
+  const marketBlock = document.getElementById('scrollButtonWrap');
   if (window.scrollY > 100) {
       header.classList.add('active');
+      marketBlock.classList.add('active');
   } else {
       header.classList.remove('active');
+      marketBlock.classList.remove('active');
   }
 });
